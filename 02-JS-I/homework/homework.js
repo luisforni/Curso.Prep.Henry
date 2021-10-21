dca4a10889ff628ee7b48575550ea8253677498c
luisforni <70423935+luisforni@users.noreply.github.com>
2021-10-21 12:51:14 -0300

    better solution

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -213,8 +213,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
     return ('Es vocal');
-  };
-  if (letra.length < 1) {
+  } else if (letra.length < 1) {
     return ('Dato incorrecto');
   };
   return 'Dato incorrecto';
